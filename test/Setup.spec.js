e18a2ba61d1f22efc3566d76ca018f770ec659b9
Alex Noble <Swinkid@googlemail.com>
2019-09-01 18:46:51 +0100

    Bugfixes

--- test/Setup.spec.js
+++ test/Setup.spec.js
@@ -2,8 +2,11 @@ import {shallow} from '@vue/test-utils'
 import Setup from '../src/components/Setup.vue'
 import {describe} from "mocha";
 
+const wrapper = shallow(Setup);
+
 describe('Setup.vue', () => {
     it('Checking H1', function () {
-        expect(true)
+        let h1 = wrapper.find('h1');
+        expect(h1.text()).toBe('Client Setup')
     });
 });
