ddb6a52dc27afbb88c98ed1454d6d384fecf98f9
Alex Noble <Swinkid@googlemail.com>
2019-09-01 20:50:10 +0100

    Fixing test

---
+++ tests/unit/Setup.spec.js
@@ -0,0 +1,11 @@
+import {shallow} from '@vue/test-utils'
+import Setup from "@/components/Setup";
+
+const wrapper = shallow(Setup);
+
+describe('Setup.vue', () => {
+    it('Checking H1', function () {
+        let h1 = wrapper.find('h1');
+        expect(h1.text()).toBe('Client Setup')
+    });
+});
