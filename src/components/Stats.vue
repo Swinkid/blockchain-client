f7163929c794c5f8a8b68cecec83585b49f2ff86
Alex Noble <Swinkid@googlemail.com>
2019-09-18 14:52:33 +0100

    Add new stuff

--- src/components/Stats.vue
+++ src/components/Stats.vue
@@ -1,6 +1,8 @@
 <template>
     <div>
         <h1>Stats</h1>
+        <p>Node Count: {{ nodes.length }}</p>
+        <p>Block Count: {{ chain.length }}</p>
     </div>
 </template>
 
@@ -16,21 +18,26 @@
             }
         },
         mounted () {
-            BlockchainService.getNodes().then((result) => {
-               if(result){
-                   this.nodes = result;
-               } else {
-                   this.nodes = [];
-               }
-            });
+            this.fetchStats();
+        },
+        methods: {
+            fetchStats(){
+                BlockchainService.getNodes().then((result) => {
+                    if(result){
+                        this.nodes = result;
+                    } else {
+                        this.nodes = [];
+                    }
+                });
 
-            BlockchainService.getChain().then((result) => {
-                if(result){
-                    this.chain = result;
-                } else {
-                    this.chain = [];
-                }
-            })
+                BlockchainService.getChain().then((result) => {
+                    if(result){
+                        this.chain = result;
+                    } else {
+                        this.chain = [];
+                    }
+                })
+            }
         }
     }
 </script>
