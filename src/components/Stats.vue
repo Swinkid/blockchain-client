d585644523842c570211a25c364b43449d842a95
Alex Noble <Swinkid@googlemail.com>
2019-09-22 15:59:51 +0100

    Add chain analysis

--- src/components/Stats.vue
+++ src/components/Stats.vue
@@ -1,9 +1,42 @@
 <template>
     <div>
-        <h1>Stats</h1>
-        <p>Nodes Online: {{ nodes.length }}</p>
-        <p>Block Count: {{ chain.length }}</p>
-        <p>Unspent Votes: </p>
+        <div v-if="!ready" class="text-center">
+            <b-row>
+                <b-col><b-spinner label="Spinning"></b-spinner></b-col>
+            </b-row>
+            <b-row>
+                <b-col><strong>Loading...</strong></b-col>
+            </b-row>
+        </div>
+
+        <div v-else>
+            <h1>Stats</h1>
+            <p>Nodes Online: {{ nodes.length }}</p>
+            <p>Block Count: {{ chain.length }}</p>
+
+            <h2>Results</h2>
+            <b-row>
+                <b-col class="col col-6" v-for="(candidate, index) in candidates"  v-model="candidates" style="margin-bottom: 1em">
+                    <b-card no-body>
+                        <b-row no-gutters>
+                            <b-col md="6">
+                                <b-card-img :src="candidate.photo" class="rounded-0"></b-card-img>
+                            </b-col>
+                            <b-col md="6">
+                                <b-card-body :title="candidate.name">
+                                    <b-card-text>
+                                        {{ candidate.party }}
+                                    </b-card-text>
+                                    <b-card-text>
+                                        <strong>Total Votes: </strong>{{ candidate.votes }}
+                                    </b-card-text>
+                                </b-card-body>
+                            </b-col>
+                        </b-row>
+                    </b-card>
+                </b-col>
+            </b-row>
+        </div>
     </div>
 </template>
 
@@ -15,13 +48,23 @@
         data() {
             return {
                 chain: [],
-                nodes: []
+                nodes: [],
+                candidates: [],
+                ready: false,
             }
         },
         created () {
+			this.candidates = BlockchainService.getCandidates();
+
+			this.candidates.forEach((candidate) => {
+				candidate.votes = 0;
+			});
+
             this.fetchChain().then(() => {
 				this.fetchNodes().then(() => {
-					this.analyzeChain(this.chain);
+					this.analyzeChain(this.chain).then((result, error) => {
+						this.ready = true;
+                    });
                 });
             });
         },
@@ -53,26 +96,23 @@
                 });
             },
             analyzeChain(chain){
-                let candidates = BlockchainService.getCandidates();
+                return new Promise((resolve, reject) => {
 
-                candidates.forEach((candidate) => {
-                	candidate.votes = 0;
-                });
+					chain.forEach((block) => {
+						block._data.forEach((transaction) => {
 
-                chain.forEach((block) => {
-                	block._data.forEach((transaction) => {
+							this.candidates.forEach((candidate) => {
 
-                		candidates.forEach((candidate) => {
+								if(candidate.key === transaction.receiver){
+									candidate.votes += 1;
+								}
 
-                            if(candidate.key === transaction.receiver){
-                            	candidate.votes += 1;
-                            }
+							})
+						});
+					});
 
-                        })
-                    });
-                });
-
-                console.log(candidates);
+					resolve(true);
+                })
             }
         }
     }
